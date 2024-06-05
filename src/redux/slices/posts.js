b3e5d3137afd131fd2fb51295afa6277b4e9915d
DariaAntt <gtfdhtruyh@gmail.com>
2024-06-05 18:27:18 +0400

    Запросы к БД на получение данных о статьях. Настройка рендеринга при загрузке статей с БД.

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const { data } = await axios.get("/posts");
+  return data;
+});
 
 const initialState = {
   posts: {
@@ -14,7 +20,24 @@ const initialState = {
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducer: {},
+  reducers: {},
+  extraReducers: {
+    //статус запроса - загрузка запроса
+    [fetchPosts.pending]: (state) => {
+      state.posts.items = [];
+      state.posts.status = "loading";
+    },
+    //статус запроса - успешно
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = "loaded";
+    },
+    //если произошла ошибка, возвращаем пустой массив
+    [fetchPosts.rejected]: (state) => {
+      state.posts.items = [];
+      state.posts.status = "error";
+    },
+  },
 });
 
 export const postsReducer = postsSlice.reducer;

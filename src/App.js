3277ce444c0eb4f404cf33e83460d0c527d9e7bf
rarayoyong <shindjinto@gmail.com>
2024-06-06 04:22:51 +0400

    Сохранение авторизации при обновлении страницы(при повторном входе).Сохранение токена

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Container from "@mui/material/Container";
+import { useDispatch, useSelector } from "react-redux";
+// import { logout } from "../../redux/slices/auth";
 
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
 
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, []);
+
   return (
     <>
       <Header />
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -15,6 +15,8 @@ export const Login = () => {
   const isAuth = useSelector(selectIsAuth);
   const dispatch = useDispatch();
 
+  // React.useEffect();
+
   const {
     register,
     handleSubmit,
@@ -28,8 +30,15 @@ export const Login = () => {
     mode: "onChange",
   });
 
-  const onSubmit = (values) => {
-    dispatch(fetchAuth(values));
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+    if (!data.payload) {
+      return alert("Не удалось авторизоваться");
+    }
+
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+    }
   };
 
   if (isAuth) {
